2c84e9a2f2e4d880d97cd8e4f7c87f652d0ad093
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-16 17:33:14 -0700

    class

--- PythonAPIPractice.py
+++ PythonAPIPractice.py
@@ -1,18 +1,42 @@
 import requests
 
+
+
+class User:
+	def __init__(self, first_name, last_name, email):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.email = email
+
+	def welcome(self):
+		print(f"Hello {first_name} {last_name}!")
+
+	def getEmail(self):
+		print(f"Your email is: {email}")
+
+
 URL = "https://reqres.in/api/users/2"
 
-userId = 2
-
-PARAMS = {'id' : userId}
-
-r = requests.get(url = URL, params = PARAMS)
+r = requests.get(url = URL)
 
 data = r.json()
 
 # email
 # first name
 # last name
-email = data['data']['email']
+data = data['data']
 
-print(email)
+first_name = data['first_name']
+email = data['email']
+last_name = data['last_name']
+
+
+
+
+janet = User(first_name, last_name, email)
+
+janet.welcome()
+janet.getEmail()
+
+
+
